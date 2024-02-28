0602e79dc02b4eb424bcc925725dfbd5c7b13f44
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-02-27 20:34:18 -0500

    nt3 feat se programan condiciones del login

---
+++ main.py
@@ -0,0 +1,16 @@
+userDB="hidrotech123"
+passwordDB="admin123*"
+
+print('software tech hidroituango')
+print('**************************')
+userName=input('Digita tu usuario: ')
+userpassword=input('Digita tu contraseña: ')
+
+if userDB==userName and passwordDB==userpassword:
+    print("Exito en su usuario")
+else:
+    print("Fallaste")
+
+
+'''print("Exito en su usuario") if userDB==userName and passwordDB==userPassword else print("Fallaste")'''
+
